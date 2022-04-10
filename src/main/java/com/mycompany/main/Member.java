d854009597267af4bb6e56383e6473c951020edd
62822 <62822@LAPTOP-US6C59H7>
2022-04-10 23:03:22 +0700

    First Commit

---
+++ src/main/java/com/mycompany/main/Member.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author 62822
+ */
+
+// sub class //
+
+            // INHERITANCE //
+
+public class Member extends StrayKids{
+    
+            // OVERRIDING //
+    
+    @Override
+    public void SKZ(){
+    System.out.println("Pulau yang banyak wisatawan nya adalah Bali");
+    }
+    
+    @Override
+    public void sekijeu(){
+    System.out.println("Dan juga banyak wisata Pulaunya");
+    }
+    
+}
+    
---
+++ src/main/java/com/mycompany/main/StrayKids.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author 62822
+ */
+public class StrayKids {
+
+// super class //
+
+
+    
+    public void SKZ(){
+    System.out.println("Indonesia adalah negara Pulau yang banyak wisatanya");
+    }
+    
+    public void sekijeu(){
+    System.out.println("Di Bali Banyak Pantai nya");}
+    }
+    
